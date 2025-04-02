45bb546f6b92dc61e35224e00c5fc910c20c6a6d
Adrien Gallouët <adrien@gallouet.fr>
2025-04-02 10:25:55 +0200

    Install binary with pip install
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

---
+++ bindings/python/setup.py
@@ -0,0 +1,57 @@
+import sys
+import stat
+import urllib.request
+import gzip
+import shutil
+import platform
+from pathlib import Path
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install_scripts import install_scripts
+
+NAME = "hfendpoint"
+VERSION = "0.1.0"
+URL = f"https://github.com/angt/hfendpoint-draft/releases/download/v{VERSION}"
+
+def get_binary_url():
+    machine = platform.machine()
+    if machine not in ["x86_64", "aarch64"]:
+        sys.exit(f"Unsupported architecture: {machine}")
+    return f"{NAME}-{machine}-linux.gz"
+
+class CustomInstallCommands(install_scripts):
+    def run(self):
+        self.mkpath(self.install_dir)
+        gz_file = get_binary_url()
+        gz_path = Path(self.install_dir) / gz_file
+        bin_path = Path(self.install_dir) / NAME
+
+        gz_url = f"{URL}/{gz_file}"
+        print(f"Downloading {gz_url}...")
+        urllib.request.urlretrieve(gz_url, gz_path)
+
+        print(f"Installing to {bin_path}...")
+        with gzip.open(gz_path, "rb") as f_in:
+            with open(bin_path, "wb") as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+        gz_path.unlink()
+        bin_path.chmod(bin_path.stat().st_mode | stat.S_IEXEC)
+
+        super().run()
+
+setup(
+    name="hfendpoint",
+    version=VERSION,
+    description="hfendpoint lib",
+    author="angt",
+    author_email="angt@hf.co",
+    python_requires=">=3.12",
+    install_requires=["msgpack"],
+    py_modules=["hfendpoint"],
+    cmdclass={
+        "develop": develop,
+        "install_scripts": CustomInstallCommands,
+    },
+    platforms=["Linux_x86_64", "Linux_aarch64"],
+)
